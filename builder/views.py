30f190c577adfd42446c41be1f2443783eb2ae20
xok9ty <125658205+xok9ty@users.noreply.github.com>
2025-08-14 22:58:09 +0300

    create view for home page

--- builder/views.py
+++ builder/views.py
@@ -1,3 +1,21 @@
+from .models import ResumeTemplate
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['popular_templates'] = ResumeTemplate.objects.all()[:3]
+        
+        context['page_title'] = 'Головна сторінка Resume Builder'
+        context['features'] = [
+            'Швидке створення резюме',
+            'Сучасні шаблони',
+            'Експорт у PDF та DOCX'
+        ]
+        
+        return context
