56a8aedc14b61e838c8e52daed0f782e8d4611cc
xok9ty <125658205+xok9ty@users.noreply.github.com>
2025-08-15 11:07:39 +0300

    create views for template preview and create resume

--- builder/urls.py
+++ builder/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, TemplateGalleryView
+from .views import HomePageView, TemplateGalleryView, TemplatePreviewView, CreateResumeView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('templates/', TemplateGalleryView.as_view(), name='template_gallery'),
+    path('templates/<int:pk>/preview/', TemplatePreviewView.as_view(), name='template_preview'),
+    path('resume/create/', CreateResumeView.as_view(), name='create_resume'), 
 ]
--- builder/views.py
+++ builder/views.py
@@ -1,7 +1,7 @@
-from .models import ResumeTemplate
+from .models import ResumeTemplate, Resume
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class HomePageView(TemplateView):
@@ -28,3 +28,17 @@ class TemplateGalleryView(ListView):
     
     def get_queryset(self):
         return ResumeTemplate.objects.all()
+
+class TemplatePreviewView(DetailView):
+    model = ResumeTemplate
+    template_name = 'template_preview.html'
+    context_object_name = 'template'
+
+class CreateResumeView(CreateView):
+    model = Resume
+    template_name = 'create_resume.html'
+    fields = ['title', 'template']  
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
